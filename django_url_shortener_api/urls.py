72eae020d719feec0523cda0b8ff7b8437d135cc
eugena <eugena.mihailikova@gmail.com>
2017-04-07 08:28:27 +0500

    initial commit

---
+++ django_url_shortener_api/urls.py
@@ -0,0 +1,15 @@
+"""django_url_shortener_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+"""
+
+from django.conf.urls import include, url
+from django.conf import settings
+
+from shortener.views import GoToLinkView
+
+urlpatterns = [
+    url(r'^(?P<code>\w{%s})' % settings.CODE_LENGTH, GoToLinkView.as_view()),
+    url(r'^', include('api.urls')),
+]
